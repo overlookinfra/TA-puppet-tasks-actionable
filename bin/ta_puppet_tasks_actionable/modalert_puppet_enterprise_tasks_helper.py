21e26b0f390b94b63824387dd4c4eb17185b0881
Chris Barker <cbarker@puppet.com>
2019-05-17 10:53:27 -0400

    Adds the submit to tasks endpoint function pie.py
    This removes a lot of the noop / victorops work for now, to be added
    back at a later date, but will call out to the orchestrator and also
    log connection errors to the stderr in the case of issues with requests

--- bin/ta_puppet_tasks_actionable/modalert_puppet_enterprise_tasks_helper.py
+++ bin/ta_puppet_tasks_actionable/modalert_puppet_enterprise_tasks_helper.py
@@ -1,11 +1,54 @@
 # encoding = utf-8
 import json
-import urllib2
 import requests
 import sys
 
+try:
+  from requests.packages.urllib3.exceptions import InsecureRequestWarning
+  requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+except ImportError:
+  import urllib3
+  urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def reqtask(node, task, token, environment, url, parameters=None):
+  headers = {'X-Authentication': token}
+
+  if parameters:
+    params = json.loads(parameters)
+  else:
+    params = {}
+
+  req = {
+    'environment' : environment,
+    'task' : task,
+    'params' : params,
+    'scope' : {
+      'nodes' : [node]
+    }
+  }
+
+  endpoint = '{}/command/task'.format(url)
+
+  #https://puppet.angrydome.org:8143/orchestrator/v1/command/task
+
+  try:
+    r = requests.post(endpoint, json=req, headers=headers, verify=False)
+  except:
+    print('Unexpected error:', sys.exc_info()[0])
+    print >> sys.stderr, 'Error: {}'.format(sys.exc_info()[0])
+    raise
+
+  if r.status_code == 202:
+    job = json.loads(r.text)['job']
+    return job
+  else:
+    raise ValueError('Unable to submit task command', r.status_code, r.text)
+
+
+
+
 def process_event(helper, *args, **kwargs):
-    
+
     victor_ops_token = helper.get_global_setting("victorops_token")
     alert_entity_id = helper.get_param("alert_entity_id")
     dropdown_list = helper.get_param("severity")
@@ -19,85 +62,16 @@ def process_event(helper, *args, **kwargs):
     pe_server = helper.get_global_setting("puppet_enterprise_server")
     pe_port = helper.get_global_setting("port")
     noop  = helper.get_param("noop")
-    
-    method = 'POST'
-    api_request = 'application/json' 
-    
-    urlpe = "https://" + pe_server + ":" + pe_port + "/orchestrator/v1/command/task"
-    
-    headers = {
-       'X-Authentication': pe_token,
-       'Content-Type': api_request
-       }
 
-    if noop == 1:
-        data = '{ "environment" : "%s", "task" : "%s" ,"description" : "%s" ,"params" :  {%s},"scope" : {"nodes" : ["%s"]}}, "noop" : "true"' % (env, state_message, description, parameters, node)
-        
-        print >> sys.stderr, "ERROR Server response: %s" % data
-    else:
-        data = '{ "environment" : "%s", "task" : "%s" ,"description" : "%s" ,"params" :  {%s},"scope" : {"nodes" : ["%s"]}}' % (env, state_message, description, parameters, node)
-    
-        print >> sys.stderr, "ERROR Server response: %s" % data
-    
-    response = requests.post(urlpe, headers=headers, data=data, verify=False)
-    
-    if victor_ops_token == "":
-        
-        print >> sys.stderr, "No VictorOps Account Setup" 
-        return
-        
-    else:
-        
-        url = "https://alert.victorops.com/integrations/generic/20131114/alert/" + victor_ops_token + ""
-            
-        
-        data = dict(
-            message_type=dropdown_list,
-            monitoring_tool='Puppet Tasks',
-            state_message=state_message,
-            job_description=description,
-            entity_id=alert_entity_id,
-            view_report=data,
-            pe_server=pe_server,
-            pe_port=pe_port,
-            node=node
-        )
-        
-        
-        search_name = helper.get_events()
-        entity_id = "Puppet Tasks Alert: %s" % search_name
-        
-        view_report = helper.get_param('results_link')
-        
-        helper.log_info("Alert action Puppet Tasks VictorOps Alert Action started.")
-        
-      
-        response = helper.send_http_request(url, 
-                                        method, 
-                                        parameters=None, 
-                                        payload=json.dumps(data),
-                                        headers=headers, 
-                                        cookies=None, 
-                                        verify=False, 
-                                        cert=None,
-                                        timeout=None, 
-                                        use_proxy=True)
-        
-        
-        body = json.dumps(data)
-        
-        req = urllib2.Request(url, body, {"Content-Type": "application/json"})
-        
-        try:
-            res = urllib2.urlopen(req)
-            body = res.read()
-            print >> sys.stderr, "INFO VictorOps server responded with HTTP status=%d" % res.code
-            print >> sys.stderr, "DEBUG VictorOps server response: %s" % json.dumps(body)
-            return 200 <= res.code < 300
-        except urllib2.HTTPError, e:
-            print >> sys.stderr, "ERROR Error sending message: %s (%s)" % (e, str(dir(e)))
-            print >> sys.stderr, "ERROR Server response: %s" % e.read()
-            return False
-         
-    
-        return 0
+    method = 'POST'
+    api_request = 'application/json'
+
+    urlpe = "https://" + pe_server + ":" + pe_port + "/orchestrator/v1"
+    helper.set_log_level(dropdown_list)
+    helper.log_info("Alert action Puppet Tasks Alert Action started.")
+    try:
+        jobid = reqtask(node, state_message,pe_token,env,urlpe)
+    except:
+        ValueError('Unable to submit task command', state_message, pe_server)
+
+    return 0
